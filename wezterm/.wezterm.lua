1277f51bcd9ab746e43a869c7d98005b86fbc6eb
Ratnadeep Bhattacharyya <ratndeepofghato@gmail.com>
2023-06-08 01:50:22 +0530

    wezterm config

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,27 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+-- config.color_scheme = 'Batman'
+config.window_background_opacity = 0.8
+config.enable_wayland = true
+config.hide_tab_bar_if_only_one_tab = true
+config.enable_tab_bar = false
+config.cursor_fg = '#000000'
+-- config.font =
+--     wezterm.font("JetBrainsMonoNL Nerd Font Mono")
+
+
+-- and finally, return the configuration to wezterm
+return config
