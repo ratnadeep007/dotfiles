ede3eb8efa121f298891e2264b978cae3331b748
Ratnadeep Bhattachryya <ratnadeepofghato@gmail.com>
2023-09-02 14:43:06 +0530

    Added better fold

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,32 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local ignore_filetypes = { "nvcheatsheet", "neo-tree", "Outline" }
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = ignore_filetypes,
+  callback = function()
+    print("HELLO")
+    -- if ignore_filetypes[vim.bo.filetype] or vim.bo.buftype == "nofile" then
+    --   require("ufo").detach()
+    --   vim.opt_local.foldenable = false
+    -- end
+    -- print("working")
+    require("ufo").detach()
+    vim.opt_local.foldenable = false
+  end,
+})
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = ignore_filetypes,
+  callback = function()
+    print("HELLO")
+    -- if ignore_filetypes[vim.bo.filetype] or vim.bo.buftype == "nofile" then
+    --   require("ufo").detach()
+    --   vim.opt_local.foldenable = false
+    -- end
+    -- print("working")
+    require("ufo").detach()
+    vim.opt_local.foldenable = false
+  end,
+})
--- nvim/lua/plugins/starter.lua
+++ nvim/lua/plugins/starter.lua
@@ -3,6 +3,43 @@
 -- if true then return {} end
 --
 
+-- require for folding
+
+-- vim.cmd([[hi FoldColumn guibg=green guifg=LightGray ctermbg=black ctermfg=LightGray]])
+
+-- Customise fold text
+local handler = function(virtText, lnum, endLnum, width, truncate)
+  local newVirtText = {}
+  local totalLines = vim.api.nvim_buf_line_count(0)
+  local foldedLines = endLnum - lnum
+  local suffix = ("󰁂  %d %d%%"):format(foldedLines, foldedLines / totalLines * 100)
+  local sufWidth = vim.fn.strdisplaywidth(suffix)
+  local targetWidth = width - sufWidth
+  local curWidth = 0
+  for _, chunk in ipairs(virtText) do
+    local chunkText = chunk[1]
+    local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+    if targetWidth > curWidth + chunkWidth then
+      table.insert(newVirtText, chunk)
+    else
+      chunkText = truncate(chunkText, targetWidth - curWidth)
+      local hlGroup = chunk[2]
+      table.insert(newVirtText, { chunkText, hlGroup })
+      chunkWidth = vim.fn.strdisplaywidth(chunkText)
+      -- str width returned from truncate() may less than 2nd argument, need padding
+      if curWidth + chunkWidth < targetWidth then
+        suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+      end
+      break
+    end
+    curWidth = curWidth + chunkWidth
+  end
+  local rAlignAppndx =
+    math.max(math.min(vim.opt.textwidth["_value"], width - 1) - curWidth - sufWidth, 0)
+  suffix = (" "):rep(rAlignAppndx) .. suffix
+  table.insert(newVirtText, { suffix, "MoreMsg" })
+  return newVirtText
+end
 
 -- lsp config for lualine
 local lsp = {
@@ -83,7 +120,6 @@ local dracula_colors = {
   yellow = "#f1fa8c",
 }
 
-vim.cmd(string.format("hi! Folded guifg=%s guibg=%s", dracula_colors.orange, colors.grey))
 -- every spec file under config.plugins will be loaded automatically by lazy.nvim
 --
 -- In your plugin files, you can:
@@ -492,40 +528,6 @@ return {
     end,
   },
   {
-    "anuvyklack/pretty-fold.nvim",
-    config = function()
-      require("pretty-fold").setup({
-        -- fill_char = "----",
-        sections = {
-          left = {
-            "+- ",
-            "content",
-          },
-          right = {
-            " ",
-            "number_of_folded_lines",
-            ": ",
-            "percentage",
-            " ",
-            function(config)
-              return config.fill_char:rep(3)
-            end,
-          },
-        },
-      })
-    end,
-  },
-  {
-    "anuvyklack/fold-preview.nvim",
-    dependencies = { "anuvyklack/keymap-amend.nvim" },
-    config = function()
-      require("fold-preview").setup({
-        auto = 500,
-        border = "rounded",
-      })
-    end,
-  },
-  {
     "folke/noice.nvim",
     event = "VeryLazy",
     opts = {
@@ -568,7 +570,7 @@ return {
   {
     "tpope/vim-fugitive",
     keys = {
-      { "<leader>fgf", "<cmd>tab Git | Git diff<cr>", desc = "Open fugitive" },
+      { "<leader>fgf", "<cmd>tab Git diff | Git<cr>", desc = "Open fugitive" },
       { "<leader>fgp", "<cmd>tab Git push<cr>", desc = "Fugittive push" },
       { "<leader>fgb", "<cmd>Git diff<cr>", desc = "Fugittive diff" },
     },
@@ -581,17 +583,17 @@ return {
       { "<leader>o", "<cmd>Oil<cr>", desc = "Oil" },
     },
   },
-  {
-    "stevearc/aerial.nvim",
-    opts = {},
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    keys = {
-      { "<leader>a", "<cmd>AerialToggle<cr>", desc = "AerialToggle" },
-    },
-  },
+  -- {
+  --   "stevearc/aerial.nvim",
+  --   opts = {},
+  --   dependencies = {
+  --     "nvim-tree/nvim-web-devicons",
+  --     "nvim-treesitter/nvim-treesitter",
+  --   },
+  --   keys = {
+  --     { "<leader>a", "<cmd>AerialToggle<cr>", desc = "AerialToggle" },
+  --   },
+  -- },
   {
     "akinsho/toggleterm.nvim",
     version = "*",
@@ -610,22 +612,56 @@ return {
       { "<leader>tf", "<cmd>ToggleTerm term://*toggleterm#* direction=float<cr>", desc = "ToggleTerm float" },
     },
   },
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = {
+      "kevinhwang91/promise-async",
+      {
+        "luukvbaal/statuscol.nvim",
+        config = function()
+          local builtin = require("statuscol.builtin")
+          require("statuscol").setup({
+            relculright = true,
+            segments = {
+              { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+              { text = { "%s" }, click = "v:lua.ScSa" },
+              { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+            },
+          })
+        end,
+      },
+    },
+    init = function()
+      vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+      vim.o.foldcolumn = "1" -- '0' is not bad
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+    end,
+    config = function()
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities.textDocument.foldingRange = {
+        dynamicRegistration = false,
+        lineFoldingOnly = true,
+      }
+      local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
+      for _, ls in ipairs(language_servers) do
+        require("lspconfig")[ls].setup({
+          capabilities = capabilities,
+          -- you can add other fields for setting up lsp server in this table
+        })
+      end
+      ---@diagnostic disable-next-line: missing-fields
+      require("ufo").setup({
+        fold_virt_text_handler = handler,
+      })
+      vim.keymap.set("n", "<C-k>", function()
+        local winid = require("ufo").peekFoldedLinesUnderCursor()
+        if not winid then
+          vim.lsp.buf.hover()
+          -- vim.cmd([[ cspsaga hover_doc ]])
+        end
+      end)
+    end,
+  },
 }
-
--- local _border = "single"
---
--- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
---   vim.lsp.handlers.hover, {
---     border = _border
---   }
--- )
---
--- vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
---   vim.lsp.handlers.signature_help, {
---     border = _border
---   }
--- )
---
--- vim.diagnostic.config{
---   float={border=_border}
--- }
