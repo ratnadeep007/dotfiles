cb628977d966a3184b13adc3fe1c84058f7b88fa
Ratnadeep Bhattacharyya <ratnadeepofghato@gmail.com>
2024-06-08 11:23:19 +0530

    added nvim plugin

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,7 +1,11 @@
-return { 
-  "echasnovski/mini.nvim", 
+return {
+  "echasnovski/mini.nvim",
   version = false,
+  dependencies = {
+    "echasnovski/mini.ai",
+  },
   config = function()
+    require("mini.ai").setup()
     require("mini.bufremove").setup()
     require("mini.pairs").setup()
     require("mini.surround").setup({
--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,6 +1,7 @@
 return {
     "stevearc/oil.nvim",
     lazy = false,
+    enable = false,
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       require("oil").setup({
