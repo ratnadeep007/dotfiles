5ea017ea25f74352e3860fb91adce2612ba4664e
Ratnadeep Bhattacharyya <ratnadeepofghato@gmail.com>
2024-06-16 12:10:20 +0530

    fix wezterm padding and tmux window inactive bg

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -47,6 +47,12 @@ config.enable_tab_bar = false
 config.font_size = 15
 config.adjust_window_size_when_changing_font_size = false
 config.line_height = 1.5
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
 -- config.font = wezterm.font("Comic Shanns Mono", { weight = "Regular"})
 
 -- and finally, return the configuration to wezterm
